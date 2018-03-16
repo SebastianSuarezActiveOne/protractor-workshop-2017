3315132884c49b8e41e8d7f0691e0dcf2876198d
Sebastian Suarez <sebastian.suarez@activeone.co>
2018-03-16 11:25:58 -0500

    Migrate to TypeScript

--- protractor/config.ts
+++ protractor/config.ts
@@ -1,7 +1,10 @@
-exports.config = {
+import { browser, Config } from "protractor";
+
+export const config: Config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['test/Google.spec.js'],
+    noGlobals: true,
+    specs: ['../test/Google.spec.js'],
     onPrepare: () => {
         browser.ignoreSynchronization = true;
     }

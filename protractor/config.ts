a67396689437b9d7ab8f3590649a4d4d485eb65c
Sebastian Suarez <sebastian.suarez@activeone.co>
2018-03-16 13:46:52 -0500

    Added static code analysis

--- protractor/config.ts
+++ protractor/config.ts
@@ -3,12 +3,12 @@ import { browser, Config } from "protractor";
 import { reporter } from "./helpers/reporter";
 
 export const config: Config = {
-    framework: 'jasmine',
-    SELENIUM_PROMISE_MANAGER: false,
-    noGlobals: true,
-    specs: ['../test/Google.spec.js'],
-    onPrepare: () => {
-        browser.ignoreSynchronization = true;
-        reporter();
-    }
+  framework: 'jasmine',
+  SELENIUM_PROMISE_MANAGER: false,
+  noGlobals: true,
+  specs: ['../test/Google.spec.js'],
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    reporter();
+  }
 }
--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -3,18 +3,18 @@ import { browser, Config } from "protractor";
 import { reporter } from "./helpers/reporter";
 
 export const config: Config = {
-    framework: 'jasmine',
-    SELENIUM_PROMISE_MANAGER: false,
-    noGlobals: true,
-    capabilities: {
-        browserName: 'chrome',
-        chromeOptions: {
-            args: ['--headless', 'disable-gpu', '--window-size=800,600']
-        }
-    },
-    specs: ['../test/Google.spec.js'],
-    onPrepare: () => {
-        browser.ignoreSynchronization = true;
-        reporter();
+  framework: 'jasmine',
+  SELENIUM_PROMISE_MANAGER: false,
+  noGlobals: true,
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', 'disable-gpu', '--window-size=800,600']
     }
+  },
+  specs: ['../test/Google.spec.js'],
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    reporter();
+  }
 }

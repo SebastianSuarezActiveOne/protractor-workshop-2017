470ab98d0668556743020fbbdfbea910b7d1a3fa
Sebastian Suarez <sebastian.suarez@activeone.co>
2018-03-22 14:50:55 -0500

    Split the describes

--- src/page/ProductDetail.page.ts
+++ src/page/ProductDetail.page.ts
@@ -1,10 +1,13 @@
-import { $, ElementFinder, promise } from 'protractor';
+import { $, ElementFinder, promise, browser, ProtractorExpectedConditions } from 'protractor';
+
+const EC: ProtractorExpectedConditions = new ProtractorExpectedConditions(browser);
 
 export class ProductDetailPage {
   private get productImage(): ElementFinder {
     return $('#center_column > ul > li > div > div.left-block > div > a.product_img_link > img');
   }
   public goToProductDetail(): promise.Promise<void> {
+    browser.wait(EC.elementToBeClickable(this.productImage));
     return this​.productImage.click();
   }
 }
--- test/BuyTshirt.spec.ts
+++ test/BuyTshirt.spec.ts
@@ -4,6 +4,13 @@ import {
   MenuContentPage, ProductDetailPage, ProductAddedPage, SummaryPage, SignInStep, AdressPage, ShippingPage, PaymentPage, OrderResumePage
 } from '../src/page';
 
+describe('Open the browser', () => {
+  it('should open the browser', async () => {
+    await browser.get('http://automationpractice.com/');
+    await expect(browser.getTitle()).toEqual("My Store");
+  });
+});
+
 describe('Buy a t-shirt', () => {
 
   const EC: ProtractorExpectedConditions = new ProtractorExpectedConditions(browser);
@@ -11,26 +18,45 @@ describe('Buy a t-shirt', () => {
   const productDetailPage: ProductDetailPage = new ProductDetailPage();
   const productAddedPage: ProductAddedPage = new ProductAddedPage();
   const summaryPage: SummaryPage = new SummaryPage();
-  const signInStep: SignInStep = new SignInStep();
-  const addressPage: AdressPage = new AdressPage();
-  const shippingPage: ShippingPage = new ShippingPage();
-  const paymentPage: PaymentPage = new PaymentPage();
-  const orderResumePage: OrderResumePage = new OrderResumePage();
+
 
   it('then should be bought a t-shirt', async () => {
-    await browser.get('http://automationpractice.com/');
     await menuContentPage.goToTShirtMenu();
-    await browser.wait(EC.elementToBeClickable($('#center_column > ul > li > div > div.left-block > div > a.product_img_link > img')));
     await productDetailPage.goToProductDetail();
     await browser.wait(EC.elementToBeClickable($('#add_to_cart > button > span')));
     await productAddedPage.addProduct();
     await browser.wait(EC.elementToBeClickable($('[style*="display: block;"] .button-container > a')));
     await productAddedPage.goToCheckout();
     await summaryPage.goToCheckout();
+    await expect($(".page-heading").getText()).toEqual("AUTHENTICATION");
+  });
+});
+
+describe('Log in', () => {
+  it('should sign in', async () => {
+    const signInStep: SignInStep = new SignInStep();
     await signInStep.fillEmail();
     await signInStep.fillPassword();
     await signInStep.signIn();
+    await expect($(".page-heading").getText()).toEqual("ADDRESSES");
+  });
+});
+
+describe('Select default address', () => {
+  it('should select default adresses', async () => {
+    const addressPage: AdressPage = new AdressPage();
     await addressPage.goToCheckout();
+    await expect($(".page-heading").getText()).toEqual("SHIPPING");
+  });
+});
+
+describe('Payment', () => {
+  it('should pay', async () => {
+
+    const shippingPage: ShippingPage = new ShippingPage();
+    const paymentPage: PaymentPage = new PaymentPage();
+    const orderResumePage: OrderResumePage = new OrderResumePage();
+
     await shippingPage.checkToS();
     await shippingPage.goToCheckout();
     await paymentPage.goToPayment();
@@ -39,4 +65,3 @@ describe('Buy a t-shirt', () => {
       .toBe('Your order on My Store is complete.');
   });
 });
-
